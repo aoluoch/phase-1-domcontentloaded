3291dab6262a02f19bfc2c1cd987738f45299209
Amos Oluoch <aoluoch271@gmail.com>
2024-07-07 20:12:49 +0300

    dom

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const textElement = document.getElementById("text");
+    textElement.textContent = "This is really cool!";
+    console.log("The DOM has loaded and the text content is updated.");
+  });
+
+  console.log("loads index.js");
+  
